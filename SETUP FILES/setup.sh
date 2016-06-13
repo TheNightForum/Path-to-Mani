366847c53376d52c6de4b1721b42a869a92b65da
crazywolf132 <crazywolf132@gmail.com>
2016-06-13 22:37:06 +1000

    Fixed setup .sh to include basic option between eclipse and intellij

--- SETUP FILES/setup.sh
+++ SETUP FILES/setup.sh
@@ -1,2 +1,20 @@
-clear
-./gradlew idea
+#!/bin/bash
+# Simple setup
+
+PS3='Please enter your editor: '
+options=("Eclipse" "Intellij" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            ./gradlew eclipse
+            ;;
+        "Option 2")
+            ./gradlew idea
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
