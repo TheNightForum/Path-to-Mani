6a016e428972653fb3355251e6ce82ec1ed4749c
crazywolf132 <crazywolf132@gmail.com>
2016-06-14 20:01:14 +1000

    Added KittoDJ to credits.

--- main/src/org/burntgameproductions/PathToMani/menu/CreditsScreen.java
+++ main/src/org/burntgameproductions/PathToMani/menu/CreditsScreen.java
@@ -58,6 +58,7 @@ public class CreditsScreen implements ManiUiScreen {
                 "Team Leader: CrazyWolf",
                 "Programmer: stur86 & CrazyWolf",
                 "Ideas: xXDahChubChubXx",
+                "Music selection: KittoDJ"
                 "Art: CrazyWolf"
             },
             {
