9c5fc8f08ee0510f11a8d418e6fb41daba8c5b4f
crazywolf132 <crazywolf132@gmail.com>
2016-06-15 15:35:41 +1000

    commented how back button works Play-Menu

--- main/src/org/burntgameproductions/PathToMani/menu/PlayScreen.java
+++ main/src/org/burntgameproductions/PathToMani/menu/PlayScreen.java
@@ -130,6 +130,7 @@ public class PlayScreen implements ManiUiScreen {
       return;
     }
 
+    //What to do when "Back" button is pressed.
     if (myBackCtrl.isJustOff()) {
       im.setScreen(cmp, screens.main);
       return;
