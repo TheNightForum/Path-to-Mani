2b0321bb37ec804ca6dcec8019fe8b19823601a4
crazywolf132 <crazywolf132@gmail.com>
2016-06-15 14:38:44 +1000

    added normal logo for changer.

--- main/src/org/burntgameproductions/PathToMani/menu/MainScreen.java
+++ main/src/org/burntgameproductions/PathToMani/menu/MainScreen.java
@@ -170,6 +170,11 @@ public class MainScreen implements ManiUiScreen {
       float sz = .55f;
       if (!DebugOptions.PRINT_BALANCE) uiDrawer.draw(aprilfools, sz, sz, sz/2, sz/2, uiDrawer.r/2, sz/2, 0, ManiColor.W);
     }
+    else
+    {
+      float sz = .55f;
+      if (!DebugOptions.PRINT_BALANCE) uiDrawer.draw(logo, sz, sz, sz/2, sz/2, uiDrawer.r/2, sz/2, 0, ManiColor.W);
+    }
   }
 
   @Override
