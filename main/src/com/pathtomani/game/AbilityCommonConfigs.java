7c0f2cd9c2575d62e306908b71bbe9d681eb02dd
crazywolf132 <crazywolf132@gmail.com>
2016-09-16 22:10:27 +1000

    Moving files to GFX.

--- main/src/com/pathtomani/game/AbilityCommonConfigs.java
+++ main/src/com/pathtomani/game/AbilityCommonConfigs.java
@@ -20,10 +20,10 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 import com.pathtomani.game.sound.SoundManager;
-import com.pathtomani.effects.GameColors;
-import com.pathtomani.effects.TextureManager;
+import com.pathtomani.gfx.GameColors;
+import com.pathtomani.gfx.TextureManager;
 import com.pathtomani.files.FileManager;
-import com.pathtomani.effects.particle.EffectTypes;
+import com.pathtomani.gfx.particle.EffectTypes;
 
 public class AbilityCommonConfigs {
     public final AbilityCommonConfig teleport;
