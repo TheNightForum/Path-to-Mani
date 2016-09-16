744fd8a83155ae105350cb4d1c47ac2969594b5e
crazywolf132 <crazywolf132@gmail.com>
2016-09-16 21:40:02 +1000

    Moving files to common.

--- main/src/com/pathtomani/game/screens/MenuScreen.java
+++ main/src/com/pathtomani/game/screens/MenuScreen.java
@@ -16,7 +16,7 @@
 
 package com.pathtomani.game.screens;
 
-import com.pathtomani.GameOptions;
+import com.pathtomani.common.GameOptions;
 import com.pathtomani.ManiApplication;
 import com.pathtomani.common.ManiColor;
 import com.pathtomani.game.ManiGame;
