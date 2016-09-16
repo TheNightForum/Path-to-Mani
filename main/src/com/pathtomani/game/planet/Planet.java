6aea3d21e2f4f37b1cd34d47b40a1871a5ed4818
crazywolf132 <crazywolf132@gmail.com>
2016-09-16 21:38:07 +1000

    Moving files to common.

--- main/src/com/pathtomani/game/planet/Planet.java
+++ main/src/com/pathtomani/game/planet/Planet.java
@@ -17,7 +17,7 @@
 package com.pathtomani.game.planet;
 
 import com.badlogic.gdx.math.Vector2;
-import com.pathtomani.Const;
+import com.pathtomani.common.Const;
 import com.pathtomani.common.Bound;
 import com.pathtomani.common.ManiMath;
 import com.pathtomani.game.HardnessCalc;
