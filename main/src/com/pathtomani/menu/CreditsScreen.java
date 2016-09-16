69ff3560ed1af7a5d421fae393aae836582036fd
crazywolf132 <crazywolf132@gmail.com>
2016-09-16 22:12:27 +1000

    Moving files to GFX.

--- main/src/com/pathtomani/menu/CreditsScreen.java
+++ main/src/com/pathtomani/menu/CreditsScreen.java
@@ -22,7 +22,7 @@ import com.pathtomani.ManiApplication;
 import com.pathtomani.common.ManiMath;
 import com.pathtomani.ui.*;
 import com.pathtomani.common.Const;
-import com.pathtomani.effects.ManiColor;
+import com.pathtomani.gfx.ManiColor;
 
 import java.util.ArrayList;
 import java.util.List;
