51bac93672d50eb4c4529bef6d9204269e61acdf
crazywolf132 <crazywolf132@gmail.com>
2016-09-17 11:10:05 +1000

    Moving files to Managers.

---
+++ main/src/com/pathtomani/screens/menu/LoadingScreen.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2016 BurntGameProductions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pathtomani.screens.menu;
+
+import com.pathtomani.ManiApplication;
+import com.pathtomani.gfx.ManiColor;
+import com.pathtomani.screens.controllers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadingScreen implements ManiUiScreen {
+  private final ArrayList<ManiUiControl> myControls;
+  private boolean myTut;
+  private boolean myUsePrevShip;
+
+  public LoadingScreen() {
+    myControls = new ArrayList<ManiUiControl>();
+  }
+
+  @Override
+  public List<ManiUiControl> getControls() {
+    return myControls;
+  }
+
+  @Override
+  public void onAdd(ManiApplication cmp) {
+  }
+
+  @Override
+  public void updateCustom(ManiApplication cmp, ManiInputManager.Ptr[] ptrs, boolean clickedOutside) {
+    cmp.startNewGame(myTut, myUsePrevShip);
+  }
+
+  @Override
+  public boolean isCursorOnBg(ManiInputManager.Ptr ptr) {
+    return false;
+  }
+
+  @Override
+  public void blurCustom(ManiApplication cmp) {
+  }
+
+  @Override
+  public void drawBg(UiDrawer uiDrawer, ManiApplication cmp) {
+  }
+
+  @Override
+  public void drawImgs(UiDrawer uiDrawer, ManiApplication cmp) {
+  }
+
+  @Override
+  public void drawText(UiDrawer uiDrawer, ManiApplication cmp) {
+    uiDrawer.drawString("Loading...", uiDrawer.r/2, .5f, FontSize.MENU, true, ManiColor.W);
+  }
+
+  @Override
+  public boolean reactsToClickOutside() {
+    return false;
+  }
+
+  public void setMode(boolean tut, boolean usePrevShip) {
+    myTut = tut;
+    myUsePrevShip = usePrevShip;
+  }
+}
