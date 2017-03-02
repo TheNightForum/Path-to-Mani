0ec84d4cfb4dd80caeff1dabde042aadf493c289
Crazywolf132 <crazywolf132@gmail.com>
2017-03-02 11:53:07 +1100

    Rebase beggins here

---
+++ main/src/org/destinationsol/game/particle/EffectType.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.destinationsol.game.particle;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
+import org.destinationsol.files.FileManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class EffectType {
+  private final ParticleEmitter myEmitter;
+  public final boolean continuous;
+  public final boolean additive;
+
+  public EffectType(String fileName) {
+    myEmitter = loadEmitter(fileName);
+    continuous = myEmitter.isContinuous();
+    myEmitter.setContinuous(false);
+    additive = myEmitter.isAdditive();
+    myEmitter.setAdditive(false);
+  }
+
+  private static ParticleEmitter loadEmitter(final String fileName) {
+    FileHandle effectFile = FileManager.getInstance().getAssetsDirectory().child("emitters").child(fileName + ".p");
+    InputStream input = effectFile.read();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(input), 512);
+    ParticleEmitter emitter;
+    try {
+      emitter = new ParticleEmitter(reader);
+    } catch (IOException ex) {
+      throw new AssertionError("Error loading effect: " + effectFile, ex);
+    } finally {
+      try {
+        reader.close();
+      } catch (IOException ignore) {
+      }
+    }
+    emitter.flipY();
+    return emitter;
+  }
+
+  public ParticleEmitter newEmitter() {
+    return new ParticleEmitter(myEmitter);
+  }
+}
---
+++ main/src/org/destinationsol/game/planet/FarSky.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2015 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.destinationsol.game.planet;
+
+import com.badlogic.gdx.math.Vector2;
+import org.destinationsol.Const;
+import org.destinationsol.game.FarObj;
+import org.destinationsol.game.SolGame;
+import org.destinationsol.game.SolObject;
+
+public class FarSky implements FarObj {
+  private final Planet myPlanet;
+
+  public FarSky(Planet planet) {
+    myPlanet = planet;
+  }
+
+  @Override
+  public boolean shouldBeRemoved(SolGame game) {
+    return false;
+  }
+
+  @Override
+  public SolObject toObj(SolGame game) {
+    return new Sky(game, myPlanet);
+  }
+
+  @Override
+  public void update(SolGame game) {
+  }
+
+  @Override
+  public float getRadius() {
+    return myPlanet.getGroundHeight() + Const.MAX_SKY_HEIGHT_FROM_GROUND;
+  }
+
+  @Override
+  public Vector2 getPos() {
+    return myPlanet.getPos();
+  }
+
+  @Override
+  public String toDebugString() {
+    return null;
+  }
+
+  @Override
+  public boolean hasBody() {
+    return false;
+  }
+}
